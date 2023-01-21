b952392125a789ce35c20e1cc9046756350dd139
exaexa <exaexa@users.noreply.github.com>
2023-01-21 09:45:39 +0000

    automatic formatting
    triggered by @exaexa on PR #4

--- src/AffineHR.jl
+++ src/AffineHR.jl
@@ -64,7 +64,7 @@ function sample(m::COBREXA.MetabolicModel, warmup::AbstractMatrix, npts::Int, it
         lmin .= maximum(ifelse.(isfinite.(lmins), lmins, -Inf32), dims = 1)
         lmax .= minimum(ifelse.(isfinite.(lmaxs), lmaxs, Inf32), dims = 1)
 
-        # generate random lambdas and compute new points 
+        # generate random lambdas and compute new points
         @cuda threads = 256 blocks = 32 TeaRNG.device_fill_rand!(lws, iter * 2 + 1)
         newpts .= pts + dirs .* (lmin .+ lws .* (lmax .- lmin))
 
